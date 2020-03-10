4025ae353db8b229c9ff143d47ea32265e056a8c
Lzduque <lzduque@hotmail.com>
2020-03-10 09:29:06 -0400

    passing all tests

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,34 +1,28 @@
 class Bottles
+    def song
+        verses 99, 0
+    end
+
     def verse number
-        if number == 99
-            expected = <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer.
-Take one down and pass it around, 98 bottles of beer on the wall.
+        if number >= 2
+            str = <<-VERSE
+#{number} bottles of beer on the wall, #{number} bottles of beer.
+Take one down and pass it around, #{number - 1} bottle#{number - 1 == 1 ? "" : "s"} of beer on the wall.
 VERSE
-        elsif number == 89
-            expected = <<-VERSE
-89 bottles of beer on the wall, 89 bottles of beer.
-Take one down and pass it around, 88 bottles of beer on the wall.
-VERSE
-        elsif number == 2
-            expected = <<-VERSE
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-    VERSE
         elsif number == 1
-            expected = <<-VERSE
-1 bottle of beer on the wall, 1 bottle of beer.
+            str = <<-VERSE
+#{number} bottle of beer on the wall, #{number} bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall.
 VERSE
-        elsif number == 0
-            expected = <<-VERSE
+        else
+            str = <<-VERSE
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.
 VERSE
         end
     end
 
-    def verses num1, num2
-        self.verse num1
+    def verses hi, lo
+        hi.downto(lo).map { |n| verse(n) }.join("\n")
     end
 end
